89feec308a9d3f3a7e330781e1f0340668ea304c
Anaxirinia Jimenez <anax131@gmail.com>
2017-12-23 18:50:37 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,29 @@
-# Your code goes here!
+class Anagram
+
+  attr_accessor :diaper, :ba, :listen, :allergy
+
+  def initialize(word)
+    @diaper = "diaper"
+    @ba = "ba"
+    @listen = "listen"
+    @allergy = "allergy"
+  end
+
+
+  def match(arr)
+    new_array = []
+      arr.each do |words|
+        if words == @diaper
+          new_array << words
+        elsif words.split('').sort.join == @ba.split('').sort.join
+          new_array << words
+        elsif words.split('').sort.join == @listen.split('').sort.join
+          new_array << words
+        elsif words.split('').sort.join == @allergy.split('').sort.join
+          new_array << words
+        end
+      end
+      new_array
+  end
+
+end
